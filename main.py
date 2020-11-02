9d68c577966d0bec052e365ce3a59f7033a78d31
Peter Svenningsson <psvenningsson@tudelft.nl>
2020-11-02 19:09:17 +0100

    Added entropy plot

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ sns.set_theme(style="darkgrid")
 # local
 from distributions import class_A, class_B, class_C
 from perception import PerceptualProcessor
+from scipy import stats
 ###########
 # GLOBALS #
 ###########
@@ -25,9 +26,9 @@ draw_speed = 0.5
 # FUNCTIONS #
 #############
 
-def render(fig, belief = None, sample_history = [], likelihood_history = []):
+def render(fig, t = None, belief = None, sample_history = [], likelihood_history = [], perception_entropy_history = [], execution_entropy_history = []):
     # Static artist
-    ax1 = fig.add_subplot(121)
+    ax1 = fig.add_subplot(221)
     for c, color in zip(distributions, distributions_colors):
         confidence_ellipse(np.array(c.params['mean']), np.array(c.params['cov']), ax1, n_std = 2.0, facecolor='none', edgecolor=color, linewidth=1.5)
 
@@ -39,9 +40,17 @@ def render(fig, belief = None, sample_history = [], likelihood_history = []):
 
     # Histogram
     if belief is not None:
-        ax2 = fig.add_subplot(122)
+        ax2 = fig.add_subplot(222)
         ax2.set(ylim=(0, 1))
         sns.barplot(x=["Correct","Proximal","Distant"], y = belief, ax = ax2, palette = distributions_colors)
+
+    if perception_entropy_history and perception_entropy_history:
+        ax3 = fig.add_subplot(223)
+        sns.lineplot(x = range(len(perception_entropy_history)), y = perception_entropy_history,  dashes=True, ax = ax3, marker='o')
+        sns.lineplot(x = range(len(execution_entropy_history)), y = execution_entropy_history, ax = ax3, marker="o")
+        ax3.set(ylim=(0, 1.5))
+        plt.legend(labels=['Perception entropy', 'Execution entropy'])
+
 
     # Draw
     ax1.axis('equal')
@@ -53,6 +62,9 @@ def main():
     # Initialization
     sample_history = []
     likelihood_history = []
+    perception_entropy_history = []
+    execution_entropy_history = []
+
     t = 0
     belief = CLS_PRIOR
 
@@ -69,13 +81,15 @@ def main():
 
         # Bayesian update
         belief = perceptual.calculate_posterior(sample, belief, distributions)
-        render(fig, belief, sample_history, likelihood_history)
+        perception_entropy_history.append( stats.entropy(belief, base = 2))
+        render(fig, t, belief, sample_history, likelihood_history, perception_entropy_history, execution_entropy_history)
 
         t += 1
 
         # Calculate transition
         belief = perceptual.transition(prior = belief)
-        render(fig, belief, sample_history, likelihood_history)
+        execution_entropy_history.append( stats.entropy(belief, base = 2))
+        render(fig, t, belief, sample_history, likelihood_history, perception_entropy_history, execution_entropy_history)
 
 
 ##########
